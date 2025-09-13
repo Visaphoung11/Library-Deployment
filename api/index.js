b65bb6ebe3fad90465bb6807679bb3c83ee9b882
viaslphoung <visalphoung22@gmail.com>
2025-09-13 10:36:10 +0700

    final

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
+// const app = require("../app");
+
+// module.exports = app;
+
 const app = require("../app");
 
+// Export the app directly for Vercel
 module.exports = app;
--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerSpec = require("./docs/swagger");
+
+// dotenv.config();
+
+// if (!process.env.JWT_SECRET) {
+//   console.warn("Warning: JWT_SECRET is not set. Login will fail.");
+// }
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/students", require("./routes/students"));
+// app.use("/api/books", require("./routes/books"));
+// app.use("/api/borrows", require("./routes/borrows"));
+// app.use("/api/dashboard", require("./routes/dashboard"));
+// app.use("/api/categories", require("./routes/categories"));
+// app.use("/api/authors", require("./routes/authors"));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// module.exports = app;
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -15,6 +43,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/students", require("./routes/students"));
 app.use("/api/books", require("./routes/books"));
